015fc2a35abe86f6c337f542ec7b7e76db6be8cd
ShaankariVoruganti <67142384+ShaankariVoruganti@users.noreply.github.com>
2023-08-11 11:04:56 +0530

    Update providers.tf

--- kub/providers.tf
+++ kub/providers.tf
@@ -1,9 +1,9 @@
 provider "azurerm" {
   features {}
-client_id= "04b09b9d-8713-47f7-9f4b-1dbeea456fdc"
-client_secret= "_Zk8Q~wWO~LUsbLFz6LKeMmk~FVoYn4Uk4F8uczc"
-tenant_id= "4c4a6eb2-1694-4a2c-859b-8f68fe6a937c"
-subscription_id="faac9286-6bb8-4387-87db-d13974580c91"
+client_id= "cc1e6eb4-fb82-4749-bfde-c9ccb523e01a"
+client_secret= "lnj8Q~dvbG~AZsXEJxiXJUHQUmSHrqyMTiHfjbVq"
+tenant_id= "d1b7bd44-674e-4209-869a-64d766f5a751""
+subscription_id="d25e7bb9-faa1-4def-a137-5dda917b09e3"
 }
 terraform {
   required_providers {
